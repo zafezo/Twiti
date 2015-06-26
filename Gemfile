1ebdbb30e365673012b6191cb5479ec9046d908b
zafezo <nazarslbdn@gmail.co>
2015-06-26 10:18:05 +0200

    Make a basic User model (include secure password)

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
-gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'bcrypt-ruby', '3.1.2'
 
 group :production do
   gem 'pg', '0.17.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
  
   get 'user/new'
-  
+
   root  'static_pages#home'
-  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signup',  to: 'user#new',            via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
