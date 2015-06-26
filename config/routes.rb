87f580d917a2aa0203b2d076a2f540fdd74069bf
zafezo <nazarslbdn@gmail.co>
2015-06-26 16:27:06 +0200

    Finish sing-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
- 
-  get 'user/new'
-
+  
+  resources :users
   root  'static_pages#home'
-  match '/signup',  to: 'user#new',            via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
