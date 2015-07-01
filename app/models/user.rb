7d8009e915d718891be1eaccd15c3077bf098bc2
zafezo <nazarslbdn@gmail.co>
2015-07-01 14:15:11 +0200

    Add user microposts:

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+  
 	before_create :create_remember_token
 	before_save { self.email = email.downcase }
 	 validates :name,  presence: true, length: { maximum: 50 }
@@ -14,6 +16,10 @@ class User < ActiveRecord::Base
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
